c3d645a35eea4ef7be28833ddbfab9b173338201
lonely platypus <maxnet663@gmail.com>
2023-08-29 12:04:10 +0300

    manager in progress

---
+++ include/manager.h
@@ -0,0 +1,23 @@
+#ifndef MANGER_H
+#define MANGER_H
+
+#include "include/converter_json.h"
+#include "include/inverted_index.h"
+#include "include/search_server.h"
+
+#include <string>
+
+class Manager {
+
+   std::string engine_name;
+   std::string engine_version;
+   std::vector<std::string> indexed_documents;
+
+
+public:
+
+
+
+};
+
+#endif //MANAGER_H
