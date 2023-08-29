1b40ab805c117a65e4866fddbe6f5a9c72331889
lonely platypus <maxnet663@gmail.com>
2023-08-29 20:28:20 +0300

    in progress

--- include/manager.h
+++ include/manager.h
@@ -1,22 +1,29 @@
 #ifndef MANGER_H
 #define MANGER_H
 
+#include <string>
+#include <memory>
+
 #include "include/converter_json.h"
 #include "include/inverted_index.h"
 #include "include/search_server.h"
 
-#include <string>
 
 class Manager {
 
    std::string engine_name;
    std::string engine_version;
+   std::filesystem::path jsons_dir;
+   std::filesystem::path resources_dir;
    std::vector<std::string> indexed_documents;
 
+   std::unique_ptr<ConverterJSON> converter;
+   std::shared_ptr<InvertedIndex> document_base;
+   std::unique_ptr<SearchServer> searchServer;
 
 public:
 
-
+    Manager();
 
 };
 
