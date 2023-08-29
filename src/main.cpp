58edbe6ae94606f2e5c9edb9c0a5805aaa24a0cc
lonely platypus <maxnet663@gmail.com>
2023-08-29 10:45:30 +0300

    repairing

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
-#include "converter_json.h"
-#include "inverted_index.h"
-#include "search_server.h"
+#include "include/converter_json.h"
+#include "include/inverted_index.h"
+#include "include/search_server.h"
 #include <iostream>
 #include <vector>
 
 int main() {
-    try {
-        InvertedIndex idx;
-        idx.UpdateDocumentBase(ConverterJSON::GetTextDocuments());
-        SearchServer srv(idx);
-        ConverterJSON::putAnswers(srv.search(ConverterJSON::GetRequests()));
-    }
-    catch (std::exception &ex) {
-        std::cout << ex.what() << std::endl;
-        return -1;
-    }
-    return 0;
+//    try {
+//        InvertedIndex idx;
+//        idx.UpdateDocumentBase(ConverterJSON::getTextDocuments());
+//        SearchServer srv(idx);
+//        ConverterJSON::putAnswers(srv.search(ConverterJSON::getRequests()));
+//    }
+//    catch (std::exception &ex) {
+//        std::cout << ex.what() << std::endl;
+//        return -1;
+//    }
+//    return 0;
 }
