6dae4d50d9a3fc6e1f5affd75d24c4b773f66832
lonely platypus <maxnet663@gmail.com>
2023-09-02 18:22:50 +0300

    RelativeIndex rank field type changed to double

--- include/search_server.h
+++ include/search_server.h
@@ -23,7 +23,7 @@ struct DocRelevance {
 
 struct RelativeIndex {
     size_t doc_id;
-    float rank;
+    double rank;
 
     bool operator==(const RelativeIndex &other) const {
         return (doc_id == other.doc_id && rank == other.rank);
