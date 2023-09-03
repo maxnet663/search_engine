2c180d053b4ceebc45223be2aaed9c6c6d81b4be
lonely platypus <maxnet663@gmail.com>
2023-09-03 17:39:45 +0300

    main_simple added

---
+++ src/main_simple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "include/converter_json.h"
+#include "include/inverted_index.h"
+#include "include/search_server.h"
+
+int main() {
+    ConverterJSON convertor;
+    InvertedIndex db;
+    db.updateDocumentBase(convertor.getTextDocuments());
+    SearchServer srv(db);
+    try {
+        convertor.putAnswers(srv.search(convertor.getRequests()));
+        std::cout << "Search done" << std::endl;
+    }
+    catch (const std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+    }
+}
