916d3f31d763ff5dc5c1f156bad22e079ae1cde8
lonely platypus <maxnet663@gmail.com>
2023-09-29 17:10:12 +0300

    documents indexing improved

--- include/inverted_index.h
+++ include/inverted_index.h
@@ -16,6 +16,7 @@ class InvertedIndex {
 
     DictionaryType freq_dictionary;
     std::mutex dict_access; // mutex to manage access to freq_dictionary
+    std::mutex print_access;
 
 public:
 
@@ -61,35 +62,11 @@ public:
 private:
 
     /**
-     * Fill freq_dictionary by the unique words from the text.
-     * Uses for multithread filling db
-     * @param text: file's text to get unique words
-     */
-    void fillDictionary(std::unordered_map<std::string, size_t> &table
-                        , size_t doc_id);
-
-    /**
      * Extract texts from each docs in input_docs list
      * @param input_docs: list of paths to documents
      * @return list of texts in the same order
      */
-    std::unordered_map<std::string, size_t> loadText(const std::string &doc_path, size_t doc_id);
-
-    /**
-     * Loads texts from file's list to dest.
-     * Dest will be cleared
-     * @param docs_paths list of paths to files
-     * @param dest destination to load
-     */
-    void indexTexts(const PathsList &docs_paths
-                       , std::vector<std::unordered_map<std::string, size_t>> dest);
-
-    void
-    fillWordsTable(std::vector<std::queue<std::string>> &texts
-                   , std::vector<std::unordered_map<std::string, size_t>> &table);
-
-    std::unordered_map<std::string, size_t>
-    makeRecord(std::queue<std::string> &text);
+    void indexText(const std::string &doc_path, size_t doc_id);
 
 };
 
