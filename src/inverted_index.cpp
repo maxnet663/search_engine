2e88c9fa512c4fed1c4e1ad91926be41420ddb01
lonely platypus <maxnet663@gmail.com>
2023-09-29 17:09:25 +0300

    include/inverted_index.h

--- src/inverted_index.cpp
+++ src/inverted_index.cpp
@@ -2,8 +2,6 @@
 
 #include <future>
 #include <thread>
-#include <iostream>
-
 #include "include/custom_functions.h"
 #include "include/formatting.h"
 #include "include/file_reader.h"
@@ -15,26 +13,21 @@ void InvertedIndex::updateDocumentBase(const PathsList &input_docs) {
         custom::print_yellow("DB: no documents to update");
         return;
     }
-
     freq_dictionary.clear();
-
-    // array of records: file id {word : occurrences }
-    std::vector<std::unordered_map<std::string, size_t>> words_table;
-    indexTexts(input_docs, words_table);
-
     std::vector<std::thread> threads_pool;
     auto threads_limit = std::thread::hardware_concurrency();
     if (!threads_limit)
-        threads_limit = 4; // default threads limit
+        threads_limit = 4; // def limit
     threads_pool.reserve(threads_limit);
-    for (size_t i = 0; i < words_table.size(); ++i) {
-        threads_pool.emplace_back(&InvertedIndex::fillDictionary
-                                 , this
-                                 , std::cref(words_table[i])
-                                 , i);
+    for (size_t i = 0; i < input_docs.size(); ++i) {
+        threads_pool.emplace_back(&InvertedIndex::indexText
+                , this
+                , std::cref(input_docs[i])
+                , i);
         if (threads_pool.size() >= threads_limit) {
-            for (auto &thread : threads_pool)
+            for (auto &thread : threads_pool) {
                 thread.join();
+            }
             threads_pool.clear();
         }
     }
@@ -45,56 +38,26 @@ const Frequency& InvertedIndex::getWordCount(const std::string &word) const {
     return it == freq_dictionary.end() ? nfound : it->second;
 }
 
-void
-InvertedIndex::fillDictionary(const std::unordered_map<std::string, size_t> &table
-                              , size_t doc_id) {
-    for (auto &record : table) {
-        std::lock_guard lock{dict_access};
-            freq_dictionary[record.first].insert({doc_id, record.second});
-    }
-}
-
-std::unordered_map<std::string, size_t>
-InvertedIndex::loadText(const std::string &doc_path, size_t doc_id) {
+void InvertedIndex::indexText(const std::string &doc_path, size_t doc_id) {
     if (std::filesystem::exists(doc_path)) {
         FileReader reader(doc_path);
         if (!reader.is_open()) {
-            std::lock_guard<std::mutex> lock(dict_access);
+            std::lock_guard<std::mutex> horn(print_access);
             custom::print_yellow("Could not open the file " + doc_path);
         } else {
-            std::unordered_map<std::string, size_t> table;
             std::string buf;
             while(reader >> buf) {
-                auto found = table.find(buf);
-                if (found == table.end())
-                    table[buf] = 1;
-                else
-                    table[buf] += 1;
+                std::lock_guard lock{dict_access};
+                if (freq_dictionary.find(buf) == freq_dictionary.end()) {
+                    freq_dictionary[buf].insert({doc_id, 1});
+                } else {
+                    freq_dictionary[buf][doc_id] += 1;
+                }
             }
-            return table;
         }
     } else {
-        std::lock_guard<std::mutex> lock(dict_access);
+        std::lock_guard<std::mutex> horn(print_access);
         custom::print_yellow(doc_path + " does not exist");
-    }
-    return { };
-}
-
-void InvertedIndex::indexTexts(const PathsList &docs_paths
-                             , std::vector<std::unordered_map<std::string, size_t>> dest) {
-    std::vector<std::future<std::unordered_map<std::string, size_t>>> loader_pool;
-    loader_pool.reserve(docs_paths.size());
-    for (auto &path : docs_paths) {
-        loader_pool.emplace_back(std::async(std::launch::async
-                                            , &InvertedIndex::loadText
-                                            , this
-                                            , path));
-    }
-    if (!dest.empty())
-        dest.clear();
-    dest.reserve(loader_pool.size());
-    for (auto &text : loader_pool) {
-        dest.emplace_back(text.get());
     }
 }
 
