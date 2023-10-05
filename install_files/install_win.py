728587b139c91bb1cf346b158c78af5307dfd0f2
lonely platypus <maxnet663@gmail.com>
2023-10-06 00:00:42 +0300

    some changes to install configuration

--- install_files/install_win.py
+++ install_files/install_win.py
@@ -7,7 +7,7 @@ import re
 # install script for windows. Its takes the installation
 # path as first argument, also, the compiler can be passed
 # as the second argument (by default GCC), and the config
-# file generator can be passed as the third  argument
+# file generator can be passed as the third argument
 # (by default MinGW Makefiles) builds the project and
 # installs the project in the passed directory
 # example: python.exe install_win.py C:\path\to\install
